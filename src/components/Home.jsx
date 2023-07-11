0570f27ce8aa4712f0f705634fb8c0ff9d9c7f77
vilkelisv <v.vilkelis97@gmail.com>
2023-07-11 19:49:36 +0300

    test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -32,70 +32,76 @@ const Home = ({ config }) => {
   }, []);
 
   return (
-    <HelmetProvider>
-      <HeadTagEditor />
-      {!loading && (
-        <div className="m-auto h-full w-full">
-          {error ? (
-            <ErrorPage
-              status={`${error.status}`}
-              title={error.title}
-              subTitle={error.subTitle}
-            />
-          ) : (
-            <>
-              <div ref={mainContainer} className={`  ${bgColor}`}>
-                <div
-                  id="main-page-container"
-                  className="grid grid-cols-1 lg:grid-cols-6 gap-6 rounded-box"
-                >
-                  <div className="grid grid-cols-1 lg:grid-cols-6 col-span-6 gap-6">
-                    <div className="grid col-span-6 lg:col-span-4 gap-6 grid-rows-4">
-                      <div className="row-span-3">
-                        <AvatarCard loading={loading} data={dataConfig.about} />
+    <>
+      <h4>test</h4>
+      <HelmetProvider>
+        <HeadTagEditor />
+        {!loading && (
+          <div className="m-auto h-full w-full">
+            {error ? (
+              <ErrorPage
+                status={`${error.status}`}
+                title={error.title}
+                subTitle={error.subTitle}
+              />
+            ) : (
+              <>
+                <div ref={mainContainer} className={`  ${bgColor}`}>
+                  <div
+                    id="main-page-container"
+                    className="grid grid-cols-1 lg:grid-cols-6 gap-6 rounded-box"
+                  >
+                    <div className="grid grid-cols-1 lg:grid-cols-6 col-span-6 gap-6">
+                      <div className="grid col-span-6 lg:col-span-4 gap-6 grid-rows-4">
+                        <div className="row-span-3">
+                          <AvatarCard
+                            loading={loading}
+                            data={dataConfig.about}
+                          />
+                        </div>
+                        <div className="row-span-1">
+                          <Skill loading={loading} data={dataConfig.skills} />
+                        </div>
                       </div>
-                      <div className="row-span-1">
-                        <Skill loading={loading} data={dataConfig.skills} />
+                      <div className="grid col-span-6 lg:col-span-2 gap-6 grid-rows-4">
+                        <div className="row-span-2">
+                          <Experience
+                            loading={loading}
+                            data={dataConfig.experiences}
+                          />
+                        </div>
+                        <div className="row-span-1">
+                          <Education
+                            loading={loading}
+                            data={dataConfig.education}
+                          />
+                        </div>
+                        <div className="row-span-1">
+                          <Certification
+                            loading={loading}
+                            data={dataConfig.certifications}
+                          />
+                        </div>
                       </div>
                     </div>
-                    <div className="grid col-span-6 lg:col-span-2 gap-6 grid-rows-4">
-                      <div className="row-span-2">
-                        <Experience
-                          loading={loading}
-                          data={dataConfig.experiences}
-                        />
-                      </div>
-                      <div className="row-span-1">
-                        <Education
-                          loading={loading}
-                          data={dataConfig.education}
-                        />
-                      </div>
-                      <div className="row-span-1">
-                        <Certification
-                          loading={loading}
-                          data={dataConfig.certifications}
-                        />
+                    <div className="grid grid-cols-6 lg:col-span-6 col-span-6 gap-6">
+                      <div className="grid col-span-6 gap-6">
+                        <AllProjects loading={loading} data={data.projects} />
                       </div>
                     </div>
-                  </div>
-                  <div className="grid grid-cols-6 lg:col-span-6 col-span-6 gap-6">
-                    <div className="grid col-span-6 gap-6">
-                      <AllProjects loading={loading} data={data.projects} />
-                    </div>
-                  </div>
-                  <div className="grid grid-cols-1 col-span-6 gap-6">
-                    <div className="grid grid-cols-1 gap-6">
-                      <Details loading={loading} data={data.details} />
+                    <div className="grid grid-cols-1 col-span-6 gap-6">
+                      <div className="grid grid-cols-1 gap-6">
+                        <Details loading={loading} data={data.details} />
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </>
-          )}
-        </div>
-      )}
-    </HelmetProvider>
+              </>
+            )}
+          </div>
+        )}
+      </HelmetProvider>
+    </>
   );
 };
 
